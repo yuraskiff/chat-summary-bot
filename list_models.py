42c33e32eab2e5549471e7a3eeab0dc51fb25340
yuraskiff <164538747+yuraskiff@users.noreply.github.com>
2025-04-12 00:51:04 +0300

    Delete list_models.py

--- list_models.py
+++
@@ -1,8 +0,0 @@
-import openai
-
-# Замените YOUR_OPENAI_API_KEY на ваш реальный API-ключ
-openai.api_key = "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"
-
-models = openai.Model.list()
-for model in models.data:
-    print(model.id)
