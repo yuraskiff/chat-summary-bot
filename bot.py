4c0a2ccd615d7420fa30a45232219831bbf0900d
yuraskiff <164538747+yuraskiff@users.noreply.github.com>
2025-04-17 15:15:57 +0300

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router
-from aiogram.types import Message
-
-router = Router()
-
-@router.message()
-async def echo_handler(message: Message):
-    await message.answer("✅ Бот работает через webhook!")
