e88d5dbf06caf2d08e02855f6e7caaace4968be0
yuraskiff <164538747+yuraskiff@users.noreply.github.com>
2025-04-25 22:49:14 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL")
