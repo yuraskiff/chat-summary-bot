4a80f8b91604b4a9e4e80cdc42a0abf6889b4eb4
yuraskiff <164538747+yuraskiff@users.noreply.github.com>
2025-04-26 00:41:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import asyncio
 from bot import dp, schedule_daily_summary
-from aiogram import Bot
-from config import TELEGRAM_TOKEN
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("bot")
-
-async def main():
-    bot = Bot(token=TELEGRAM_TOKEN)
-    schedule_daily_summary()
-    logger.info("🤖 Бот запущен и ожидает сообщения...")
-    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    from aiogram import Bot
+    from config import TELEGRAM_TOKEN
+
+    bot = Bot(token=TELEGRAM_TOKEN, parse_mode="HTML")
+    schedule_daily_summary()
+    asyncio.run(dp.start_polling(bot))
