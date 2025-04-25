2aa796bb974528f516185d1b1fd854a3e695b0a5
yuraskiff <164538747+yuraskiff@users.noreply.github.com>
2025-04-26 00:50:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import asyncio
+import logging
+from aiogram import Bot
 from bot import dp, schedule_daily_summary
+from config import TELEGRAM_TOKEN
 
 async def main():
     print("🤖 Бот запускается...")
-    schedule_daily_summary()  # Запускаем планировщик внутри event loop
-    await dp.start_polling()
+    bot = Bot(token=TELEGRAM_TOKEN, parse_mode="HTML")
+    schedule_daily_summary(bot)
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
