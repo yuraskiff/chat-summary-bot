4d3a55a42f88c3375b2d0600892812d6f00f2aed
yuraskiff <164538747+yuraskiff@users.noreply.github.com>
2025-05-04 04:38:30 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-# Скрипт запуска бота (Render.com)
+
+# start.sh - Пример скрипта запуска
+
+echo "Запуск приложения Python..."
+
+# Переходим в директорию с main.py, если нужно
+# cd /opt/render/project/src/
+
+# Запускаем основной скрипт
 python main.py
